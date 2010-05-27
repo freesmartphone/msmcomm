8676cabff5572e38dbb38459b60a2f14b2aa086d
Simon Busch <morphis@gravedo.de>
2010-05-27 10:03:45 +0200

    libmsmcomm: specs: convert xml specification to sdef format

--- libmsmcomm/generate_structures.sh
+++ libmsmcomm/generate_structures.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+tools/xml-to-sdef.py specs/xml/call.xml specs/xml/sim.xml specs/xml/network.xml specs/xml/system.xml specs/xml/audio.xml specs/xml/gps.xml > specs/msmcomm.sdef
 tools/generate_structs.py specs/msmcomm.sdef > src/structures.h
