6685eeeb3ba3ee72b799657e4539fcffe98fa96f
Simon Busch <morphis@gravedo.de>
2010-05-28 20:07:30 +0200

    libmsmcomm: move structure definitions from specs into structures directory

--- libmsmcomm/generate_structures.sh
+++ libmsmcomm/generate_structures.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-tools/xml-to-sdef.py specs/xml/call.xml specs/xml/sim.xml specs/xml/network.xml specs/xml/system.xml specs/xml/audio.xml specs/xml/gps.xml > specs/msmcomm.sdef
+tools/xml-to-sdef.py specs/structures/call.xml specs/structures/sim.xml specs/structures/network.xml specs/structures/system.xml specs/structures/audio.xml specs/structures/gps.xml > specs/msmcomm.sdef
 tools/generate_structs.py specs/msmcomm.sdef > src/structures.h
