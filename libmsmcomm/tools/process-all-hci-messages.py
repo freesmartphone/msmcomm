7137154f38e73dcf45027e908b27213b50de0e35
Simon Busch <morphis@gravedo.de>
2010-11-15 10:18:45 +0100

    libmsmcomm: fix bug in process-all-hci-messages.py script
    We have to be carefull with the results from the all_hci_messages files as their order is
    not the right to 100%.

--- libmsmcomm/tools/process-all-hci-messages.py
+++ libmsmcomm/tools/process-all-hci-messages.py
@@ -14,9 +14,14 @@ with open(sys.argv[1]) as f:
     if current_group == "":
       current_group = parts[0]
     elif not current_group == parts[0]:
+      print ""
       current_group = parts[0]
       current_group_id += 1
       current_message_id = 0x0
+
+      # Special handling for PDSM group; There is one group (0x20) missing!
+      if current_group == "HCI_SUBSYS_PDSM_CMD":
+        current_group_id += 1
 
     print "%s, %s, 0x%02x, 0x%02x" % (parts[0], parts[1], current_group_id, current_message_id)
 
