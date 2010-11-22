b01f1a0130e115d0057f7775487ee0f8e096afd4
Simon Busch <morphis@gravedo.de>
2010-11-22 09:28:58 +0100

    libmsmcomm: update gsdi events

--- libmsmcomm/src/event_gsdi.c
+++ libmsmcomm/src/event_gsdi.c
@@ -1,4 +1,6 @@
 /*
+ * vim: noai:ts=4:sw=4:expandtab
+ *
  * (c) 2010 by Simon Busch <morphis@gravedo.de>
  * All Rights Reserved
  *
@@ -28,7 +30,7 @@ extern void *talloc_msmc_ctx;
 
 unsigned int event_sim_status_is_valid(struct msmcomm_message *msg)
 {
-    return (msg->group_id == 0x11);
+    return (msg->group_id == MSMCOMM_MESSAGE_GROUP_EVENT_GSDI);
 }
 
 void event_sim_status_handle_data(struct msmcomm_message *msg, uint8_t * data, uint32_t len)
@@ -51,96 +53,111 @@ msmcomm_message_type_t event_sim_status_get_type(struct msmcomm_message *msg)
 
     switch (msg->msg_id)
     {
-        case 0:
+        case 0x00:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_INSERTED;
-        case 9:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_VERIFIED;
-        case 10:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_BLOCKED;
-        case 11:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_UNBLOCKED;
-        case 12:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_ENABLED;
-        case 13:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_DISABLED;
-        case 14:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_CHANGED;
-        case 15:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_PERM_BLOCKED;
-        case 16:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_VERIFIED;
-        case 17:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_BLOCKED;
-        case 18:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_UNBLOCKED;
-        case 19:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_ENABLED;
-        case 20:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_DISABLED;
-        case 21:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_CHANGED;
-        case 22:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_PERM_BLOCKED;
-        case 26:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_RESET;
-        case 27:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_INIT;
-        case 57:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_INIT_FCN;
-        case 61:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_FAILED;
-        case 28:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_FDN_ENABLE;
-        case 29:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_FDN_DISABLE;
-        case 66:
-            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_ILLEGAL;
-        case 1:
+        case 0x01:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REMOVED;
-        case 5:
+        case 0x05:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_NO_SIM_EVENT;
-        case 6:
+        case 0x06:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_NO_EVENT;
-        case 2:
+        case 0x02:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_DRIVER_ERROR;
-        case 67:
-        case 68:
-        case 69:
-        case 70:
-        case 71:
+        case 0x09:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_VERIFIED;
+        case 0x0a:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_BLOCKED;
+        case 0x0b:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_UNBLOCKED;
+        case 0x0c:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_ENABLED;
+        case 0x0d:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_DISABLED;
+        case 0x0e:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_CHANGED;
+        case 0x0f:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN1_PERM_BLOCKED;
+        case 0x10:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_VERIFIED;
+        case 0x11:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_BLOCKED;
+        case 0x12:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_UNBLOCKED;
+        case 0x13:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_ENABLED;
+        case 0x14:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_DISABLED;
+        case 0x15:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_CHANGED;
+        case 0x16:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PIN2_PERM_BLOCKED;
+        case 0x1a:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_RESET;
+        case 0x1b:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_INIT;
+        case 0x39:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_INIT_FCN;
+        case 0x3d:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_FAILED;
+        case 0x1c:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_FDN_ENABLE;
+        case 0x1d:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_FDN_DISABLE;
+        case 0x42:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_ILLEGAL;
+        case 0x43:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_ILLEGAL_2;
+        case 0x44:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_UHZI_V2_COMPLETE;
+        case 0x45:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_UHZI_V1_COMPLETE;
+        case 0x46:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PERSO_EVENT_GEN_PROP2;
+        case 0x47:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_INTERNAL_RESET;
-        case 24:
+        case 0x18:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_OK_FOR_TERMINAL_PROFILE_DL;
-        case 25:
+        case 0x19:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_NOT_OK_FOR_TERMINAL_PROFILE_DL;
-        case 8:
+        case 0x08:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_INIT_COMPLETED_NO_PROV;
-        case 4:
+        case 0x04:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_MEMORY_WARNING;
-        case 73:
-        case 74:
+        case 0x49:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_CARD_ERR_NOT_ATR_RECEIVED_WITH_MAX_VOLTAGE;
+        case 0x50:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REAL_RESET_FAILURE;
-        case 3:
+        case 0x03:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_CARD_ERROR;
-        case 23:
+        case 0x17:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_NO_EVENT;
-        case 114:
+        case 0x72:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_GET_PERSO_NW_FAILURE;
-        case 124:
+        case 0x76:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_GET_PERSO_NW_BLOCKED;
-        case 108:
+        case 0x6c:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_APP_RESET;
-        case 109:
+        case 0x6d:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_REFRESH_3G_SESSION_RESET;
-        case 110:
-        case 111:
-        case 112:
+        case 0x6e:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_APP_RESET;
+        case 0x6f:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_3G_SESSION_RESET_2;
+        case 0x70:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_APP_SELECTED;
-        case 134:
-        case 135:
-        case 136:
-        case 137:
-        case 138:
+        case 0x86:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PERSO_SANITY_ERROR;
+        case 0x87:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PERSO_GEN_PROP1;
+        case 0x88:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PERSO_GEN_PROP2;
+        case 0x89:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PERSO_EVT_INIT_COMPLETED;
+        case 0x8a:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PLMN_SEL_MENU_ENABLE;
+        case 0x8b:
+            return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_PLMN_SEL_MENU_DISABLE;
+        case 0x90:
             return MSMCOMM_MESSAGE_TYPE_EVENT_SIM_DEFAULT;
         default:
             break;
