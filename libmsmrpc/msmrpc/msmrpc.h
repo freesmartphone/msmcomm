e90e0d57bebb16cb76bc1d175214100206510176
Simon Busch <morphis@gravedo.de>
2011-06-09 18:06:30 +0200

    libmsmrpc: add all necessary includes of header files to the common header file

--- libmsmrpc/msmrpc/msmrpc.h
+++ libmsmrpc/msmrpc/msmrpc.h
@@ -1,5 +1,14 @@
 #ifndef MSMRPC_H_
 #define MSMRPC_H_
 
+#include <msmrpc/clnt.h>
+#include <msmrpc/debug.h>
+#include <msmrpc/pmap_clnt.h>
+#include <msmrpc/rpc.h>
+#include <msmrpc/rpc_router_ioctl.h>
+#include <msmrpc/svc.h>
+#include <msmrpc/types.h>
+#include <msmrpc/xdr.h>
+
 #endif
 
